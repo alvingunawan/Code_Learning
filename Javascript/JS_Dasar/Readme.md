8e3388a8f3d39ee397bae894b69000c0a4994667
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-01-19 09:58:21 +0700

    Delete Javascript/JS_Dasar directory

--- Javascript/JS_Dasar/Readme.md
+++
@@ -1,5 +0,0 @@
-# Javascript Dasar
-Dasar Pemrograman Javascript ada disini :
-1. Konsep While
-2. Konsep For
-3. Konsep Pengulangan if
--- Javascript/JS_Dasar/angkot1better.js
+++
@@ -1,7 +0,0 @@
-var noAngkot = 1;
-var jmlAngkot = 10;
-
-while (noAngkot <= jmlAngkot) {
-  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
-  noAngkot++;
-}
