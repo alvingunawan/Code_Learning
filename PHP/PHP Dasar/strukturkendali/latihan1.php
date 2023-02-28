da5452110ae8db3ae5abfba403437586b3cee5f4
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-02-28 21:31:46 +0700

    menambahkan struktur kendali

---
+++ PHP/PHP Dasar/strukturkendali/latihan1.php
@@ -0,0 +1,24 @@
+<?php
+// Pengulangan
+// for
+// while
+// do
+// do.. while
+// foreach : pengulangan khusus uarray
+
+for($i = 0; $i <5; $i++){
+    echo "Hello World! <br>";
+}
+
+$i = 0;
+while ($i<5){
+    echo "SHello World! <br>";
+    $i++;
+}
+
+$i = 10;
+do{
+    echo "ZHello Worlds! <br>";
+    $i++;
+} while($i < 5)
+?>
