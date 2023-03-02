7d87fbf519167be7d29de3aad03109336522c2cc
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-02 17:43:18 +0700

    menambahkan pagination

---
+++ PHP/PHP Dasar/pagination/index.php
@@ -0,0 +1,100 @@
+<?php 
+    session_start();
+    // jika user belum login, kembalikan ke login.php
+    if(!isset($_SESSION["login"]) ){
+        header("location: login.php");
+        exit;
+    }
+
+    require 'functions.php';
+
+    // pagination
+    // konfigurasi
+    $jumlahDataPerHalaman = 3;
+    $jumlahData = count(query("SELECT * FROM mahasiswa"));
+    $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+
+    //jika halamannya ada isinya, halaman aktifnya diambil dari URL
+    //kalau !isset/belum ditulis apapun di URLnya, halaman aktifnya di set 1 (asumsikan 1)
+    $halamanAktif = (isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1; //operator ternary
+
+    $awalData = ($jumlahDataPerHalaman * $halamanAktif ) - $jumlahDataPerHalaman;  
+
+    $mahasiswa = query("SELECT * FROM mahasiswa LIMIT $awalData, $jumlahDataPerHalaman");
+
+    //tombol cari diklik
+    if(isset($_POST["cari"])){
+        $mahasiswa = cari($_POST["keyword"]);
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword pencarian" autocomplete="off">
+        <button type="submit" name="cari">Cari !</button>
+    </form>
+    <br><br>
+
+    <!-- Navigasi -->
+    <?php if($halamanAktif > 1) : ?>
+        <a href="?halaman=<?= $halamanAktif-1 ?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for($i = 1; $i <= $jumlahHalaman; $i++): ?>
+        <?php if($i == $halamanAktif): ?>
+            <a href="?halaman=<?= $i ?>" style="font-weight: bold; color:red"> <?= $i ?></a>
+        <?php else: ?>
+            <a href="?halaman=<?= $i ?>"> <?= $i ?></a>
+        <?php endif; ?>
+    <?php endfor;  ?>
+
+    <?php if($halamanAktif < $jumlahHalaman) : ?>
+        <a href="?halaman=<?= $halamanAktif+1 ?>">&raquo;</a>
+    <?php endif; ?>
+
+    <br><br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Nim</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <?php $i = 1 ?>
+        <?php foreach($mahasiswa as $row) : ?>     
+        <tr>
+            <td><?= $i?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"]?>">ubah</a> |
+                <a href="hapus.php?id=<?= $row["id"]?>">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"]?>" alt="Vin" width="50"></td>
+            <td><?= $row["nim"]?></td>
+            <td><?= $row["nama"]?></td>
+            <td><?= $row["email"]?></td>
+            <td><?= $row["jurusan"]?></td>  
+        </tr>
+        <?php $i++ ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
