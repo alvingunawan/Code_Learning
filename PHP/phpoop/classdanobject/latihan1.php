b4e9058f9d52cc1126bbb8c498c280a4c6be8ec4
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-10 11:51:56 +0700

    menambahkan class dan object

---
+++ PHP/phpoop/classdanobject/latihan1.php
@@ -0,0 +1,13 @@
+<?php 
+    class Coba{
+
+    }
+
+    // membuat object instances dari class coba
+    $a = new Coba();
+    $b = new Coba();
+    $c = new Coba();
+
+    var_dump($a);
+    var_dump($b);
+    var_dump($c);
