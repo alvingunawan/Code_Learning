2adde717722cbc110d7d244a0cc7497f0ab5e25e
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-11 09:44:00 +0700

    mengupdate objecttype

--- PHP/phpoop/objecttype.php
+++ PHP/phpoop/objecttype.php
@@ -22,6 +22,14 @@ class produk {
     }
 }
 
+
+class cetakInfoProduk{
+    public function cetak(produk $produk){
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
 $produk1 =  new produk("Naruto","Masashi Kishimoto","Shonen Jump", 30000);
 $produk2 = new produk("Riviera The Promised Land", "Sunaho Tobe", "Sony Pictures", 250000);
 
@@ -30,4 +38,8 @@ echo "<br>";
 echo "Komik : " .$produk1->getLabel();
 echo "<br>";
 echo "Gambar : " .$produk2->getLabel();
+echo "<br>";
+
+$infoProduk1 = new cetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
 ?>
