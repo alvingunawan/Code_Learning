de816e7e613af05ce76e013602ff0552e8eda887
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-11 10:34:57 +0700

    menambahkan inheritance-problem

---
+++ PHP/phpoop/inheritance-problem.php
@@ -0,0 +1,58 @@
+<?php 
+
+class produk {
+    public $judul, 
+            $penulis,
+            $penerbit,
+            $harga,
+            $jmlHalaman,
+            $waktuMain,
+            $tipe;
+
+    public function sayHello(){
+        return "Hello World!";
+    }
+
+    public function __construct($judul = "judul",$penulis = "penulis",$penerbit="penerbit",$harga=0, $jmlHalaman=0, $waktuMain=0, $tipe){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap(){
+        $str = "";
+        $str = "{$this->tipe} : {$this->judul} | {$this->penulis}, {$this->penerbit}, (Rp. {$this->harga})";
+        if($this->tipe == "Komik"){
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        } else if ($this->tipe == "Game"){
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
+        return $str;
+    }
+}
+
+
+class cetakInfoProduk{
+    public function cetak(produk $produk){
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+$produk1 =  new produk("Naruto","Masashi Kishimoto","Shonen Jump", 30000, 100, 0, "Komik");
+$produk2 = new produk("Riviera The Promised Land", "Sunaho Tobe", "Sony Pictures", 250000, 0, 50, "Game");
+
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+// Game : Riviera The Promisedf Land | Sunaho Tobe, Sony Picture (Rp. 250000) ~ 50 Jam.
+
+echo $produk1->getInfoLengkap();
+echo "<br>";
+echo $produk2->getInfoLengkap();
