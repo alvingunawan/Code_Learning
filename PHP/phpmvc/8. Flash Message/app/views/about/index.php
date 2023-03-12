224a0b022ab98c28bc3fb6d810f0179739d10c88
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-12 21:21:59 +0700

    menambahkan Flash Message

---
+++ PHP/phpmvc/8. Flash Message/app/views/about/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="hero bg-success-subtle">
+        <h1 class="display-4">About Me!</h1>
+        <img src="<?=BASEURL?>/img/luna.jpg" alt="Luna" width="200" class="rounded-circle shadow">
+        <p class="lead">Halo, nama saya <?= $data['nama']?>, umur saya <?= $data['umur']?> tahun, saya adalah seorang <?= $data['pekerjaan']?><i/p>
+        <hr class="-my4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate non distinctio veniam, animi vitae deleniti.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Pelajari Lebih Lanjut</a>
+    </div>
+</div>
