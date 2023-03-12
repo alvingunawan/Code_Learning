8105a5ab147a564dcf65d90d3c7cb33c1a2e70b0
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-12 17:23:32 +0700

    menambahkan assets

---
+++ PHP/phpmvc/4. Assets/app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="hero bg-success-subtle">
+        <h1 class="display-4 ">Selamat Datang di website Saya!</h1>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente impedit cum amet distinctio quis nam aut. Eius pariatur nam autem.</p>
+        <hr class="-my4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate non distinctio veniam, animi vitae deleniti.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Pelajari Lebih Lanjut</a>
+    </div>
+</div>
