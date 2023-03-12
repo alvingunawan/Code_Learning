a5a0185c56f6e0ee1a8dc3b26d8a285aa238bf05
Alvin Gunawan <57095971+alvingunawan@users.noreply.github.com>
2023-03-12 22:54:44 +0700

    menambahkan Searching Data

---
+++ PHP/phpmvc/11. Searching Data/app/views/templates/footer.php
@@ -0,0 +1,6 @@
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="<?=BASEURL?>/js/bootstrap.js"></script>
+<script src="<?=BASEURL?>/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+</html>
